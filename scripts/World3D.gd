81a48656de770a1a4280f36ac69ac965d457d4cf
03Mario <t.mario2004@gmail.com>
2026-05-31 13:51:27 +0200

    g

--- scripts/World3D.gd
+++ scripts/World3D.gd
@@ -1,14 +1,4 @@
 extends Node3D
 
-@export var player_scene: PackedScene
-
 func _ready() -> void:
 	LimboManager.register_world(self)
-
-	var existing := get_tree().get_first_node_in_group("Player")
-	if existing == null and player_scene != null:
-		var p := player_scene.instantiate()
-		add_child(p)
-		var spawn := find_child("SpawnDefault", true, false) as Node3D
-		if spawn:
-			p.global_position = spawn.global_position
