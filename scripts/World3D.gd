a37bfd4eef2bf647c3b1d94a2e1f3613a968b246
03Mario <t.mario2004@gmail.com>
2026-05-31 13:35:46 +0200

    popravak malo

--- scripts/World3D.gd
+++ scripts/World3D.gd
@@ -1,17 +1,10 @@
-## World3D.gd
-## Prikvači na root node svake scene (Node3D).
-## Registrira scenu u LimboManageru i spawnira igrača ako ga već nema.
-
 extends Node3D
 
-## Povuci Player.tscn ovdje u Inspectoru
 @export var player_scene: PackedScene
 
 func _ready() -> void:
 	LimboManager.register_world(self)
 
-	# Spawnaj igrača samo ako već nije u sceni
-	# (pri teleportu, igrač dolazi zajedno s tranzicijom)
 	var existing := get_tree().get_first_node_in_group("Player")
 	if existing == null and player_scene != null:
 		var p := player_scene.instantiate()
